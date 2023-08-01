39ce1e07e0e80f4ff0bba2a1923c3ed1b63df488
leolim <dudwnssss@naver.com>
2023-08-01 17:54:35 +0900

    TableView_(Refactor: Enum, Computed Property)

--- week3/TableView/TableView/ViewControllers/Case2TableViewController.swift
+++ week3/TableView/TableView/ViewControllers/Case2TableViewController.swift
@@ -9,19 +9,11 @@ import UIKit
 
 class Case2TableViewController: UITableViewController{
     
-    enum SettingType: Int, CaseIterable {
-        case total
-        case personal
-        case etc
-    }
-    
     let settings = SettingType.allCases
-    let totalSettingList = ["공지사항", "실험실", "버전 정보"]
-    let personalSettingList = ["개인/보안", "알림", "채팅", "프로필"]
-    let etcSettingList = ["고객센터/도움말"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 44
         setProperties()
     }
     
@@ -36,33 +28,12 @@ extension Case2TableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let setting = SettingType(rawValue: section){
-            switch setting {
-            case .total:
-                return totalSettingList.count
-            case .personal:
-                return personalSettingList.count
-            case .etc:
-                return etcSettingList.count
-            }
-        }
-        
-        return 0
+        return settings[section].settingDetail.count
     }
     
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if let setting = SettingType(rawValue: section){
-            switch setting {
-            case .total:
-                return "전체설정"
-            case .personal:
-                return "개인설정"
-            case .etc:
-                return "기타"
-            }
-        }
-        return nil
+        return settings[section].settingTitle
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,22 +44,8 @@ extension Case2TableViewController {
         cell.textLabel?.font = .systemFont(ofSize: 14)
         cell.textLabel?.textColor = .white
         cell.backgroundColor = .black
-
-        if let setting = SettingType(rawValue: indexPath.section){
-            switch setting {
-            case .total:
-                cell.textLabel?.text = totalSettingList[indexPath.row]
-            case .personal:
-                cell.textLabel?.text = personalSettingList[indexPath.row]
-            case .etc:
-                cell.textLabel?.text = etcSettingList[indexPath.row]
-            }
-        }
+        cell.textLabel?.text = settings[indexPath.section].settingDetail[indexPath.row]
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        print(#fileID, #function, #line, "- ")
-        return 44
-    }
 }
