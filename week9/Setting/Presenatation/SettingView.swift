784502090dd8c50545d9ce6f93614f614436622b
leolim <dudwnssss@naver.com>
2023-09-15 20:06:29 +0900

    Setting_(refactor_separate View from ViewController)

--- week9/Setting/Presenatation/SettingView.swift
+++ week9/Setting/Presenatation/SettingView.swift
@@ -8,13 +8,9 @@
 import UIKit
 
 class SettingView: BaseView{
-    
-    let settingViewModel = SettingViewModel()
-    let settingList = SettingType.allCases
-    
+        
     lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
     
-    var dataSource: UICollectionViewDiffableDataSource<SettingType, Setting>!
     
     private func createLayout() -> UICollectionViewLayout{
         var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
@@ -22,42 +18,6 @@ class SettingView: BaseView{
         configuration.backgroundColor = .black
         let layout = UICollectionViewCompositionalLayout.list(using: configuration)
         return layout
-    }
-    
-    private func configureDataSource(){
-        //collectionView.register
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Setting> { cell, indexPath, itemIdentifier in
-            
-            var content = UIListContentConfiguration.valueCell()
-            content.text = itemIdentifier.itemTitle
-            content.textProperties.color = .white
-            cell.contentConfiguration = content
-            
-            var backgroundConfig = UIBackgroundConfiguration.listPlainCell()
-            backgroundConfig.backgroundColor = .black
-            cell.backgroundConfiguration = backgroundConfig
-        }
-        
-        //cellForItemAt
-        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
-            return cell
-        })
-    }
-    
-    override func setProperties() {
-        configureDataSource()
-        setSnapshot()
-    }
-    
-    //numbersOfItemsInSection
-    func setSnapshot(){
-        var snapshot = NSDiffableDataSourceSnapshot<SettingType, Setting>()
-        snapshot.appendSections(SettingType.allCases)
-        snapshot.appendItems(settingViewModel.totalSettingList, toSection: .total)
-        snapshot.appendItems(settingViewModel.personalSettingList, toSection: .personal)
-        snapshot.appendItems(settingViewModel.etcSettingList, toSection: .etc)
-        dataSource.apply(snapshot)
     }
     
     override func setLayouts() {
--- week9/Setting/Presenatation/SettingViewController.swift
+++ week9/Setting/Presenatation/SettingViewController.swift
@@ -7,16 +7,53 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: BaseViewController {
     
     let settingView = SettingView()
+    let settingViewModel = SettingViewModel()
 
-    
+    var dataSource: UICollectionViewDiffableDataSource<SettingType, Setting>!
     
     override func loadView() {
         self.view = settingView
     }
     
+    override func setProperties() {
+        configureDataSource()
+        setSnapshot()
+    }
  
 }
 
+extension SettingViewController {
+    
+    private func setSnapshot(){
+        var snapshot = NSDiffableDataSourceSnapshot<SettingType, Setting>()
+        snapshot.appendSections(SettingType.allCases)
+        snapshot.appendItems(settingViewModel.totalSettingList, toSection: .total)
+        snapshot.appendItems(settingViewModel.personalSettingList, toSection: .personal)
+        snapshot.appendItems(settingViewModel.etcSettingList, toSection: .etc)
+        dataSource.apply(snapshot)
+    }
+    
+    private func configureDataSource(){
+        //collectionView.register
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Setting> { cell, indexPath, itemIdentifier in
+            
+            var content = UIListContentConfiguration.valueCell()
+            content.text = itemIdentifier.itemTitle
+            content.textProperties.color = .white
+            cell.contentConfiguration = content
+            
+            var backgroundConfig = UIBackgroundConfiguration.listPlainCell()
+            backgroundConfig.backgroundColor = .black
+            cell.backgroundConfiguration = backgroundConfig
+        }
+        
+        //cellForItemAt
+        dataSource = UICollectionViewDiffableDataSource(collectionView: settingView.collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
+    }
+}
